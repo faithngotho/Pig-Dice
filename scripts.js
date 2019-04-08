5da5a74a629fc5ab0ee9dd44437029fa63eb3b3d
faithngotho <faithwangari248@gmail.com>
2019-04-08 11:39:22 +0300

    add name added to the scripts.js file

--- scripts.js
+++ scripts.js
@@ -16,7 +16,7 @@ function Player(turn) {
 
 Player.prototype.rollone = function() {
   if (this.roll === 1) {
-    this.tempscore = 0;
+    this.rollscore = 0;
     alert("Sorry " + this.playerName + ", you rolled a 1! Your turn is over!")
 
   } else {
